405519dc8c2906ec9e7c9d24618d1a6d37a7ff40
TheSuspect9702 <92857768+TheSuspect9702@users.noreply.github.com>
2022-05-25 18:32:40 +0200

    Update Animal.java

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 public class Animal {
     private int actionPoints;
     private int x, y;
@@ -9,4 +10,17 @@ public class Animal {
         this.y = y;
         this.isHungry = true;
     }
+    private void move() {
+        Random rand = new Random();
+        if (this.isHungry == true) {
+            //value of direction will decide in which direction our animal will proceed
+            int direction = rand.nextInt(3);
+            if (direction == 0 && areaMap[x++][y].movingCost <= this.actionPoints)
+                this.x++;
+            else if (direction == 1 && areaMap[x][y++].movingCost <= this.actionPoints) 
+                this.y++;
+            else if (direction == 2 && areaMap[x--][y].movingCost <= this.actionPoints) 
+                this.x--;
+            else if (direction == 3 && areaMap[x][y--].movingCost <= this.actionPoints)
+                this.y--;
 }
