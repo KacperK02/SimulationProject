157a36d1a06c6ae00af9bee0c7885b4a64237595
TheSuspect9702 <92857768+TheSuspect9702@users.noreply.github.com>
2022-05-25 19:02:57 +0200

    Update Dirt.java

--- src/main/java/Dirt.java
+++ src/main/java/Dirt.java
@@ -1,6 +1,14 @@
+import java.util.Random;
 public class Dirt extends Area{
     public Dirt(int movingCost, int x, int y) {
         super(movingCost, x, y);
         movingCost = 1;
     }
-}
+    public void grow(){
+        Random rand = new Random();
+        int chance = rand(10);
+        if (chance < 1) 
+        areaMap[this.x][this.y] = new Grass();
+    // giving 10% chance for grass to regrow on dirt spots
+    }
+}    
