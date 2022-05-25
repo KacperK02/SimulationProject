3c8173b5d02a30089670acc75b18be409f4797e8
TheSuspect9702 <92857768+TheSuspect9702@users.noreply.github.com>
2022-05-25 18:53:39 +0200

    Update Zebra.java

--- src/main/java/Zebra.java
+++ src/main/java/Zebra.java
@@ -2,4 +2,8 @@ public class Zebra extends Animal{
     public Zebra(int actionPoints, int x, int y, boolean isHungry) {
         super(actionPoints, x, y, isHungry);
     }
+    public void eat() {
+        
+        areaMap[this.x][this.y] = new Dirt();
+    }
 }
