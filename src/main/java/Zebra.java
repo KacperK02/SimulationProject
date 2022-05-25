18675713a3c47d25ea33065de559bb9a88528e7c
TheSuspect9702 <92857768+TheSuspect9702@users.noreply.github.com>
2022-05-25 19:27:04 +0200

    Update Zebra.java

--- src/main/java/Zebra.java
+++ src/main/java/Zebra.java
@@ -3,7 +3,7 @@ public class Zebra extends Animal{
         super(actionPoints, x, y, isHungry);
     }
     public void eat() {
-        
-        areaMap[this.x][this.y] = new Dirt();
+        if (isHungry == true && areaMap[this.x][this.y] == // tu trzeba to polepszyc jakos przyrownac warunek kiedy na danym polu jest trawa
+        areaMap[this.x][this.y] = new Dirt(); // to jest opcja na pominiecie metody remove z klasy grass to juz jak uwazasz
     }
 }
