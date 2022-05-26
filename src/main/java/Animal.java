9ca4dcc10a19ca391a60fecb5da54b6d640df719
TheSuspect9702 <263937@student.pwr.edu.pl>
2022-05-26 18:03:40 +0200

    Poprawienie kodu

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -10,19 +10,32 @@ public class Animal {
         this.y = y;
         this.isHungry = true;
     }
-    private void move(Area[][] areaMap) {
+    private void move(Area[][] areaMap, Animal[][] animalMap,int x,int y) {
         Random rand = new Random();
         if (this.isHungry) {
             //value of direction will decide in which direction our animal will proceed
             int direction = rand.nextInt(3);
-            if (direction == 0 && areaMap[x++][y].movingCost <= this.actionPoints)
+            if (direction == 0 && areaMap[x+1][y].movingCost <= this.actionPoints && animalMap[x+1][y]==null) {
+                this.actionPoints = this.actionPoints - areaMap[x+1][y].movingCost;
                 this.x++;
-            else if (direction == 1 && areaMap[x][y++].movingCost <= this.actionPoints) 
+            }
+            else if (direction == 1 && areaMap[x][y+1].movingCost <= this.actionPoints && animalMap[x][y+1]==null) {
+                this.actionPoints = this.actionPoints - areaMap[x][y+1].movingCost;
                 this.y++;
-            else if (direction == 2 && areaMap[x--][y].movingCost <= this.actionPoints) 
+            }
+            else if (direction == 2 && areaMap[x-1][y].movingCost <= this.actionPoints && animalMap[x-1][y]==null) {
+                this.actionPoints = this.actionPoints - areaMap[x-1][y].movingCost;
                 this.x--;
-            else if (areaMap[x][y--].movingCost <= this.actionPoints)
+            }
+            else if (areaMap[x][y-1].movingCost <= this.actionPoints && animalMap[x][y-1]==null) {
+                this.actionPoints = this.actionPoints - areaMap[x][y-1].movingCost;
                 this.y--;
+            }
+        }
+    }
+    private void die(Animal[][] animalMap){
+        if (this.actionPoints == 0 && this.isHungry) {
+            animalMap[this.x][this.y] = null;
         }
     }
 }
