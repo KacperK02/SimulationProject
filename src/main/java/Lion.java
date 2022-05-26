7c7c410ba1697de4f178e3af385586b5c7f241b4
TheSuspect9702 <263937@student.pwr.edu.pl>
2022-05-26 18:17:09 +0200

    Poprawienie kodu

--- src/main/java/Lion.java
+++ src/main/java/Lion.java
@@ -2,4 +2,22 @@ public class Lion extends Animal{
     public Lion(int actionPoints, int x, int y, boolean isHungry) {
         super(actionPoints, x, y, isHungry);
     }
+    public void eat (Animal animalMap[][]){
+        if (this.isHungry && animalMap[this.x+1][this.y] == ) { //do poprawy jak u zebry
+            this.isHungry = false;
+            animalMap[this.x+1][this.y] = null;
+        }
+        else if (this.isHungry && animalMap[this.x][this.y+1] == ) {
+            this.isHungry = false;
+            animalMap[this.x][this.y+1] = null;
+        }
+        else if (this.isHungry && animalMap[this.x][this.y-1] == ) {
+            this.isHungry = false;
+            animalMap[this.x][this.y-1] = null;
+        }
+        else if (this.isHungry && animalMap[this.x-1][this.y] == ) {
+            this.isHungry = false;
+            animalMap[this.x-1][this.y] = null;
+        }
+    }
 }
