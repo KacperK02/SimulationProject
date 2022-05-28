324f6defc617512f8bce349e670fcb1001bdab67
Kacper Krzesinski <k.krzesinski02@gmail.com>
2022-05-28 11:59:54 +0200

    Poprawiono warunki w Zebra i Lion, dodano metody w Main

--- src/main/java/Lion.java
+++ src/main/java/Lion.java
@@ -3,19 +3,19 @@ public class Lion extends Animal{
         super(actionPoints, x, y, isHungry);
     }
     public void eat (Animal animalMap[][]){
-        if (this.isHungry && animalMap[this.x+1][this.y] == ) { //do poprawy jak u zebry
+        if (this.isHungry && animalMap[this.x+1][this.y] instanceof Zebra) {
             this.isHungry = false;
             animalMap[this.x+1][this.y] = null;
         }
-        else if (this.isHungry && animalMap[this.x][this.y+1] == ) {
+        else if (this.isHungry && animalMap[this.x][this.y+1] instanceof Zebra ) {
             this.isHungry = false;
             animalMap[this.x][this.y+1] = null;
         }
-        else if (this.isHungry && animalMap[this.x][this.y-1] == ) {
+        else if (this.isHungry && animalMap[this.x][this.y-1] instanceof Zebra ) {
             this.isHungry = false;
             animalMap[this.x][this.y-1] = null;
         }
-        else if (this.isHungry && animalMap[this.x-1][this.y] == ) {
+        else if (this.isHungry && animalMap[this.x-1][this.y] instanceof Zebra ) {
             this.isHungry = false;
             animalMap[this.x-1][this.y] = null;
         }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,18 +3,12 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
-    public static void main(String args[]){
-        System.out.print("Podaj rozmiar mapy: ");
-        Scanner scanner = new Scanner(System.in);
-        int size = scanner.nextInt();
+    public static void GenerateAnimalMap(Animal animalMap[][], int size, Area areaMap[][]){
         Random rand = new Random();
-        //Inicjalizacja tablic w obiekty
-        Animal animalMap[][] = new Animal[size][size];
-        Area areaMap[][] = new Area[size][size];
         for (int i=0; i<size;i++) {
             for (int j=0;j<size;j++) {
                 int chance = rand.nextInt(6);
-                if (chance == 1)
+                if (chance == 1 && !(areaMap[i][j] instanceof Mountain))
                     animalMap[i][j] = new Lion (5,i,j,true);
                 else if (chance == 2)
                     animalMap[i][j] = new Zebra (5,i,j,true);
@@ -22,18 +16,36 @@ public class Main {
                     animalMap[i][j] = null;
             }
         }
+    }
+
+    public static void GenerateAreaMap(Area areaMap[][], int size){
+        Random rand = new Random();
         for (int i=0; i<size;i++) {
             for (int j = 0; j < size; j++) {
                 int chance = rand.nextInt(6);
                 if (chance == 0)
-                    areaMap[i][j] = new Dirt(1, i, j);
+                    areaMap[i][j] = new Grass(1, i, j);
                 else if (chance == 1)
                     areaMap[i][j] = new Mountain(9, i, j);
                 else if (chance == 2)
                     areaMap[i][j] = new Sand(2, i, j);
                 else
-                    areaMap[i][j] = new Grass(1, i, j);
+                    areaMap[i][j] = new Dirt(1, i, j);
             }
         }
+    }
+    public static void main(String args[]){
+        System.out.print("Podaj rozmiar mapy: ");
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        Random rand = new Random();
+
+        //Inicjalizacja tablic w obiekty
+        Animal animalMap[][] = new Animal[size][size];
+        Area areaMap[][] = new Area[size][size];
+
+        //Generowanie map
+        GenerateAnimalMap(animalMap, size, areaMap);
+        GenerateAreaMap(areaMap, size);
     }
 }
